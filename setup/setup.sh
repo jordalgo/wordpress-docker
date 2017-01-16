12e1b0901384c52f77e70c9042431b5668c2fe73
Jordalgo <jordalgo@spotify.com>
2017-01-16 07:00:34 -0800

    Improve the setup workflow

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -e
 
-cp setup/package.json .
-cp setup/composer.json .
-cp setup/gulpfile.js .
-cp setup/htaccess-local .htaccess
-cp setup/deploy.sh .
-cp setup/deploy-exclude-list.txt .
+customized_files=(
+  'composer.json'
+  'deploy.sh'
+  'deploy-exclude-list.txt'
+  'gulpfile.js'
+  'package.json'
+)
+
+for i in ${customized_files[@]}; do
+  echo Copying ${i} from setup to root and removing it from gitignore
+  sed -i '' -e '/'${i}'/d' ./.gitignore
+  cp setup/${i} .
+
+done
+echo "Finished!"
+exit 0
+
